73ff4b7b237df8780a68d895d77b7f85b89caacb
richard-devbot <honeyricky1m3@gmail.com>
2025-08-26 10:22:00 +0530

    Fix langchain version to use stable 0.3.27 instead of dev version

--- chaos_hunter/utils/constants.py
+++ chaos_hunter/utils/constants.py
@@ -1,16 +1,16 @@
 from PIL import Image
 import numpy as np
 
-SKAFFOLD_YAML_TEMPLATE_PATH = "chaoseater/data_generation/templates/skaffold_yaml_template.j2"
-UNITTEST_BASE_PY_PATH = "chaoseater/ce_tools/k8s/unittest_base.py"
-K6_POD_TEMPLATE_PATH = "chaoseater/ce_tools/k6/templates/k6_pod_template.j2"
-K8S_POD_TEMPLATE_PATH = "chaoseater/ce_tools/k8s/templates/k8s_pod_template.j2"
-META_TEMPLATE_PATH  = "chaoseater/ce_tools/chaosmesh/templates/workflow_meta_template.j2"
-TASK_TEMPLATE_PATH  = "chaoseater/ce_tools/chaosmesh/templates/task_template.j2"
-TASK_K6_TEMPLATE_PATH = "chaoseater/ce_tools/chaosmesh/templates/task_k6_template.j2"
-FAULT_TEMPLATE_PATH = "chaoseater/ce_tools/chaosmesh/templates/fault_template.j2"
-GROUNDCHILDREN_TEMPLATE_PATH = "chaoseater/ce_tools/chaosmesh/templates/groundchildren_template.j2"
-SUSPEND_TEMPLATE_PATH = "chaoseater/ce_tools/chaosmesh/templates/suspend_template.j2"
+SKAFFOLD_YAML_TEMPLATE_PATH = "chaos_hunter/data_generation/templates/skaffold_yaml_template.j2"
+UNITTEST_BASE_PY_PATH = "chaos_hunter/ce_tools/k8s/unittest_base.py"
+K6_POD_TEMPLATE_PATH = "chaos_hunter/ce_tools/k6/templates/k6_pod_template.j2"
+K8S_POD_TEMPLATE_PATH = "chaos_hunter/ce_tools/k8s/templates/k8s_pod_template.j2"
+META_TEMPLATE_PATH  = "chaos_hunter/ce_tools/chaosmesh/templates/workflow_meta_template.j2"
+TASK_TEMPLATE_PATH  = "chaos_hunter/ce_tools/chaosmesh/templates/task_template.j2"
+TASK_K6_TEMPLATE_PATH = "chaos_hunter/ce_tools/chaosmesh/templates/task_k6_template.j2"
+FAULT_TEMPLATE_PATH = "chaos_hunter/ce_tools/chaosmesh/templates/fault_template.j2"
+GROUNDCHILDREN_TEMPLATE_PATH = "chaos_hunter/ce_tools/chaosmesh/templates/groundchildren_template.j2"
+SUSPEND_TEMPLATE_PATH = "chaos_hunter/ce_tools/chaosmesh/templates/suspend_template.j2"
 
 CHAOSHUNTER_LOGO_PATH = "docs/static/images/image.png"
 CHAOSHUNTER_IMAGE_PATH = "docs/static/images/chashunter_icon.png"
